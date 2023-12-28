59827aa340db4c1cd51d1707ea2d7876c08ef347
Jun <117130912+Duckyoubeaver@users.noreply.github.com>
2023-12-28 23:16:19 +1100

    commit navbar

---
+++ components/ui/Navbar/Account.tsx
@@ -0,0 +1,38 @@
+// AccountDropdown.js
+'use client';
+
+import s from './Navbar.module.css';
+import SignOutButton from './SignOutButton';
+import { useState } from 'react';
+
+const AccountDropdown = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="relative">
+      <button
+        onClick={toggleDropdown}
+        className="text-sm focus:outline-none py-1" // Removed bg-gray-200
+        aria-label="Account"
+      >
+        Account
+      </button>
+      {isOpen && (
+        <div className="absolute right-0 mt-2 w-56 bg-black border border-gray-300 rounded-md shadow-lg text-white">
+          {' '}
+          {/* Changed background to black and text color to white */}
+          <div className="py-10 flex items-center justify-center">
+            <SignOutButton />
+            <button className={`${s.link} text-sm `}>Upgrade</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AccountDropdown;
--- components/ui/Navbar/Navbar.tsx
+++ components/ui/Navbar/Navbar.tsx
@@ -1,10 +1,11 @@
-import Link from 'next/link';
-import { createServerSupabaseClient } from '@/app/supabase-server';
-
-import Logo from '@/components/icons/Logo';
-import SignOutButton from './SignOutButton';
-
+import AccountDropdown from './Account';
 import s from './Navbar.module.css';
+import SignOutButton from './SignOutButton';
+import { createServerSupabaseClient } from '@/app/supabase-server';
+import Logo from '@/components/icons/Logo';
+import Link from 'next/link';
+
+//Use server component here, but where client side interaction is needded use a client component
 
 export default async function Navbar() {
   const supabase = createServerSupabaseClient();
@@ -17,32 +18,82 @@ export default async function Navbar() {
       <a href="#skip" className="sr-only focus:not-sr-only">
         Skip to content
       </a>
-      <div className="max-w-6xl px-6 mx-auto">
-        <div className="relative flex flex-row justify-between py-4 align-center md:py-6">
+      <div className="max-w-8xl px-6 mx-auto">
+        {/* New section above existing elements */}
+        <div className="flex justify-between py-5 align-center">
+          <div className="flex items-center">
+            <span className="text-sm font-bold">Organization Name</span>
+          </div>
+          <div className="flex items-center space-x-4">
+            <input
+              type="text"
+              placeholder="Search"
+              className={`${s.link} text-sm`}
+            />
+            <button
+              className={`${s.link} text-sm border border-gray-300 rounded-md px-3 py-1`}
+            >
+              Feedback
+            </button>
+            {/* {user && (
+              <Link href="/account" className={`${s.link} text-sm`}>
+                Account
+              </Link>
+            )} */}
+            <div className="flex justify-end flex-1 space-x-8">
+              {user ? (
+                <AccountDropdown />
+              ) : (
+                <Link href="/signin" className={`${s.link} text-sm`}>
+                  Sign in
+                </Link>
+              )}
+            </div>
+            {/* {user && <AccountDropdown />} Use the new component here */}
+          </div>
+        </div>
+
+        {/* Existing navigation section */}
+        <div className="relative flex flex-row justify-between py-2 align-center md:py-0">
           <div className="flex items-center flex-1">
-            <Link href="/" className={s.logo} aria-label="Logo">
+            <Link href="/" className={`${s.logo} text-sm`} aria-label="Logo">
               <Logo />
             </Link>
             <nav className="hidden ml-6 space-x-2 lg:block">
-              <Link href="/" className={s.link}>
-                Pricing
-              </Link>
               {user && (
-                <Link href="/account" className={s.link}>
-                  Account
+                <Link href="/account" className={`${s.link} text-sm`}>
+                  Overview
                 </Link>
               )}
+              {user && (
+                <Link href="/account" className={`${s.link} text-sm`}>
+                  Analytics
+                </Link>
+              )}
+              {user && (
+                <Link href="/account" className={`${s.link} text-sm`}>
+                  Data Sources
+                </Link>
+              )}
+              {user && (
+                <Link href="/account" className={`${s.link} text-sm`}>
+                  Storage
+                </Link>
+              )}
+              <Link href="/" className={`${s.link} text-sm`}>
+                Pricing
+              </Link>
             </nav>
           </div>
-          <div className="flex justify-end flex-1 space-x-8">
+          {/* <div className="flex justify-end flex-1 space-x-8">
             {user ? (
               <SignOutButton />
             ) : (
-              <Link href="/signin" className={s.link}>
+              <Link href="/signin" className={`${s.link} text-sm`}>
                 Sign in
               </Link>
             )}
-          </div>
+          </div> */}
         </div>
       </div>
     </nav>
