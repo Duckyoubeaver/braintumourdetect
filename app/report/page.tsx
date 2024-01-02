7b6d1e8ef65b2297c3a0bce1465e3de80ed701bb
Jun <117130912+Duckyoubeaver@users.noreply.github.com>
2024-01-02 20:48:58 +1100

    Auto-commit

--- app/report/page.tsx
+++ app/report/page.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Overview = () => {
   return (
-    <div className="w-screen h-screen bg-white mt-20">
-      <div className="bg-black h-screen w-50"></div>
+    <div className="w-screen h-screen bg-gray-700 flex items-center justify-center">
+      <div className="bg-white h-4/5 w-4/6 p-8 shadow-md rounded-lg">
+        {/* Content inside the sheet of paper */}
+        <input
+          type="text"
+          className="w-full h-full bg-transparent outline-none border-none text-lg text-black"
+          placeholder=""
+        />
+      </div>
     </div>
   );
 };
