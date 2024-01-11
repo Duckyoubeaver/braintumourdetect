20aa70409b679598d59c798d447ca8ec21afd153
Jun <wonjunoh0708@gmail.com>
2024-01-11 19:15:00 +1100

    commit bitch

--- components/ui/Navbar/Account.tsx
+++ components/ui/Navbar/Account.tsx
@@ -17,13 +17,13 @@ const AccountDropdown = () => {
     <div className="relative">
       <button
         onClick={toggleDropdown}
-        className={`${s.link} text-xs p-2`} // Removed bg-gray-200
+        className={`${s.link} text-xs p-2 z-100`} // Removed bg-gray-200
         aria-label="Account"
       >
         Account
       </button>
       {isOpen && (
-        <div className="absolute right-0 mt-2 w-56 bg-white rounded-md shadow-lg text-white">
+        <div className="absolute right-0 z-20 mt-2 w-56 bg-white rounded-md shadow-lg text-white">
           {' '}
           {/* Changed background to black and text color to white */}
           <div className="py-2 flex items-center justify-center">
